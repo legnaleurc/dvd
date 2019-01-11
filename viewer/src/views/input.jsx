93538561321e1984b4bb96f2250a214ca74ae6db
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-01-11 11:52:54 +0900

    add forwared props for input widget

--- viewer/src/views/input.jsx
+++ viewer/src/views/input.jsx
@@ -8,7 +8,12 @@ export default function Input (props) {
     <input
       className="input"
       type={props.type}
+      value={props.value}
+      defaultValue={props.defaultValue}
+      onChange={props.onChange}
+      onChangeCapture={props.onChangeCapture}
       onKeyPress={props.onKeyPress}
+      onKeyPressCapture={props.onKeyPressCapture}
     />
   );
 };
