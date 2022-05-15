8e93978865a5196dd363c5d96668238bfb623b95
Wei-Cheng Pan <legnaleurc@gmail.com>
2022-05-15 23:35:00 +0900

    add patched type

--- viewer/src/patch.d.ts
+++ viewer/src/patch.d.ts
@@ -3,3 +3,26 @@
 interface DataTransfer {
   mozUserCancelled: boolean;
 }
+
+// Partial type to fix this:
+// https://github.com/v1ack/svelte-virtual-scroll-list/pull/2
+declare module "svelte-virtual-scroll-list" {
+  import { SvelteComponentTyped } from "svelte";
+
+  export interface VirtualScrollProps<T extends Record<string, unknown>> {
+    data?: T[];
+    estimateSize?: number;
+  }
+
+  export default class VirtualScroll<
+    T extends Record<string, unknown>,
+  > extends SvelteComponentTyped<
+    VirtualScrollProps<T>,
+    Record<string, never>,
+    {
+      default: { data: T };
+      footer: Record<string, never>;
+      header: Record<string, never>;
+    }
+  > {}
+}
