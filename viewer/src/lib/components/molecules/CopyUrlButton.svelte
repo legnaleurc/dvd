51c5dfbbf24164bfec19bd2a50a440094854eeb5
Wei-Cheng Pan <legnaleurc@gmail.com>
2022-05-23 12:50:33 +0900

    refactor copy url

--- viewer/src/lib/components/molecules/CopyUrlButton.svelte
+++ viewer/src/lib/components/molecules/CopyUrlButton.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
   import { getStreamUrl } from "$lib/tools/api";
-  import { getSelectionContext } from "$lib/stores/selection";
   import Icon from "$lib/components/atoms/Icon.svelte";
   import IconButton from "$lib/components/atoms/IconButton.svelte";
 
   export let isFolderById: (id: string) => boolean;
   export let getNameById: (id: string) => string;
+  export let selectedId: Set<string>;
 
-  const { selectedId } = getSelectionContext();
-
-  $: isSelectionEmpty = $selectedId.size <= 0;
+  $: isSelectionEmpty = selectedId.size <= 0;
 
   async function handleCopyUrl() {
-    const idList = Array.from($selectedId);
+    const idList = Array.from(selectedId);
     const fileList = idList.filter((id) => !isFolderById(id));
     const urlList = fileList.map((id) => {
       const name = getNameById(id);
--- viewer/src/lib/components/organisms/file/DesktopToolBar.svelte
+++ viewer/src/lib/components/organisms/file/DesktopToolBar.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   import { getFileSystemContext } from "$lib/stores/filesystem";
+  import { getSelectionContext } from "$lib/stores/selection";
+  import CopyUrlButton from "$lib/components/molecules/CopyUrlButton.svelte";
   import RenameButton from "$lib/components/organisms/widget/RenameButton.svelte";
   import TrashButton from "$lib/components/organisms/widget/TrashButton.svelte";
   import DeselectAllButton from "$lib/components/organisms/widget/DeselectAllButton.svelte";
   import InternalImageButton from "$lib/components/organisms/widget/InternalImageButton.svelte";
-  import CopyUrlButton from "$lib/components/organisms/widget/CopyUrlButton.svelte";
   import DownloadButton from "$lib/components/organisms/widget/DownloadButton.svelte";
   import InternalVideoButton from "$lib/components/organisms/widget/InternalVideoButton.svelte";
   import SortButton from "./SortButton.svelte";
   import CreateFolderButton from "./CreateFolderButton.svelte";
 
   const { nodeMap, sync } = getFileSystemContext();
+  const { selectedId } = getSelectionContext();
 
   function getNameById(id: string) {
     return $nodeMap[id]?.name ?? "";
@@ -43,7 +45,7 @@
       on:aftercreate={handleAfterAction}
     />
     <DownloadButton {getNameById} {isFolderById} />
-    <CopyUrlButton {getNameById} {isFolderById} />
+    <CopyUrlButton {getNameById} {isFolderById} selectedId={$selectedId} />
   </div>
   <div class="flex-1" />
   <div class="flex-0 flex flex-col">
