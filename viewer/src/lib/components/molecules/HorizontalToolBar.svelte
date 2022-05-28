8ab98675f3beb40883197f069f3088f48dfdbfcf
Wei-Cheng Pan <legnaleurc@gmail.com>
2022-05-29 08:07:36 +0900

    change design

--- viewer/src/lib/components/molecules/HorizontalToolBar.svelte
+++ viewer/src/lib/components/molecules/HorizontalToolBar.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import type { SvelteCustomEvents } from "$lib/types/traits";
+  import Icon from "$lib/components/atoms/Icon.svelte";
+  import IconButton from "$lib/components/atoms/IconButton.svelte";
   import DeselectAllButton from "./DeselectAllButton.svelte";
+  import DownloadButton from "./DownloadButton.svelte";
   import ExternalOpenButton from "./ExternalOpenButton.svelte";
   import InternalImageButton from "./InternalImageButton.svelte";
   import InternalVideoButton from "./InternalVideoButton.svelte";
@@ -24,22 +27,42 @@
   export let openComic: (id: string, name: string) => void;
   export let openVideo: (id: string) => void;
   export let shortcutList: string[];
+
+  let expand = false;
+
+  function toggleMenu() {
+    expand = !expand;
+  }
 </script>
 
-<div class="flex bg-paper-800">
-  <div class="flex-0 flex">
-    <DeselectAllButton {selectedId} {deselectAll} />
-    <TrashButton {selectedId} on:trash />
+<div class="flex flex-col bg-paper-800">
+  <div class="flex" class:hidden={!expand}>
+    <div class="flex-1" />
+    <div class="flex-0 flex">
+      <CopyUrlButton {getNameById} {isFolderById} {selectedId} />
+      <DownloadButton {getNameById} {isFolderById} {selectedId} />
+      <ExternalOpenButton {getNameById} {getMimeTypeById} {selectedId} />
+      <InternalVideoButton {selectedId} {deselectAll} {openVideo} />
+    </div>
   </div>
-  <div class="flex-1" />
-  <div class="flex-0 flex overflow-x-auto overflow-y-hidden">
-    <CopyUrlButton {getNameById} {isFolderById} {selectedId} />
-    <ExternalOpenButton {getNameById} {getMimeTypeById} {selectedId} />
-    <InternalVideoButton {selectedId} {deselectAll} {openVideo} />
-  </div>
-  <div class="flex-0 flex">
-    <RenameButton {getNameById} {selectedId} on:rename />
-    <ShortcutButton {selectedId} {shortcutList} on:move />
-    <InternalImageButton {getNameById} {selectedId} {deselectAll} {openComic} />
+  <div class="flex">
+    <div class="flex-0 flex">
+      <DeselectAllButton {selectedId} {deselectAll} />
+      <IconButton on:click={toggleMenu}>
+        <Icon name={expand ? "expand_less" : "expand_more"} />
+      </IconButton>
+      <TrashButton {selectedId} on:trash />
+    </div>
+    <div class="flex-1" />
+    <div class="flex-0 flex">
+      <RenameButton {getNameById} {selectedId} on:rename />
+      <ShortcutButton {selectedId} {shortcutList} on:move />
+      <InternalImageButton
+        {getNameById}
+        {selectedId}
+        {deselectAll}
+        {openComic}
+      />
+    </div>
   </div>
 </div>
