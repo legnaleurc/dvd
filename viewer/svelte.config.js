fd47046a1440e129acd290dde71a8de0b43abe7d
Wei-Cheng Pan <legnaleurc@gmail.com>
2022-06-11 07:32:26 +0900

    server side redirect

--- viewer/svelte.config.js
+++ viewer/svelte.config.js
@@ -4,6 +4,26 @@ import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 import { splitVendorChunkPlugin } from "vite";
 
+/**
+ * @param {Record<string, string>} map
+ * @returns {import('vite').PluginOption}
+ */
+const redirect = (map) => ({
+  name: "redirect",
+  configureServer: (server) => () => {
+    server.middlewares.use((req, res, next) => {
+      if (map[req.url]) {
+        res.statusCode = 302;
+        res.setHeader("Location", map[req.url]);
+        res.setHeader("Content-Length", "0");
+        res.end();
+      } else {
+        next();
+      }
+    });
+  },
+});
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -20,7 +40,7 @@ const config = {
           "/api": process.env.BACKEND_BASE_URL,
         },
       },
-      plugins: [splitVendorChunkPlugin()],
+      plugins: [redirect({ "/": "/files" }), splitVendorChunkPlugin()],
     },
   },
 };
