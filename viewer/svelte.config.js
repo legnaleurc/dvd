6c1bab986bb4e3e3a212eee9ce16e6a39ec87391
Wei-Cheng Pan <legnaleurc@gmail.com>
2022-07-20 23:06:51 +0900

    apply upgrade

--- viewer/svelte.config.js
+++ viewer/svelte.config.js
@@ -1,31 +1,8 @@
-import process from "process";
-
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
-import { splitVendorChunkPlugin } from "vite";
-
-/**
- * @param {Record<string, string>} map
- * @returns {import('vite').PluginOption}
- */
-const redirect = (map) => ({
-  name: "redirect",
-  configureServer: (server) => () => {
-    server.middlewares.use((req, res, next) => {
-      if (map[req.url]) {
-        res.statusCode = 302;
-        res.setHeader("Location", map[req.url]);
-        res.setHeader("Content-Length", "0");
-        res.end();
-      } else {
-        next();
-      }
-    });
-  },
-});
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
@@ -34,15 +11,5 @@ const config = {
     adapter: adapter({
       fallback: "index.html",
     }),
-    vite: {
-      server: {
-        proxy: {
-          "/api": process.env.BACKEND_BASE_URL,
-        },
-      },
-      plugins: [redirect({ "/": "/files" }), splitVendorChunkPlugin()],
-    },
   },
 };
-
-export default config;
---
+++ viewer/vite.config.mjs
@@ -0,0 +1,32 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import { splitVendorChunkPlugin } from "vite";
+
+/**
+ * @param {Record<string, string>} map
+ * @returns {import('vite').PluginOption}
+ */
+const redirect = (map) => ({
+  name: "redirect",
+  configureServer: (server) => () => {
+    server.middlewares.use((req, res, next) => {
+      if (map[req.url]) {
+        res.statusCode = 302;
+        res.setHeader("Location", map[req.url]);
+        res.setHeader("Content-Length", "0");
+        res.end();
+      } else {
+        next();
+      }
+    });
+  },
+});
+
+/** @type {import('vite').UserConfig} */
+export default {
+  server: {
+    proxy: {
+      "/api": process.env.BACKEND_BASE_URL,
+    },
+  },
+  plugins: [sveltekit(), redirect({ "/": "/files" }), splitVendorChunkPlugin()],
+};
