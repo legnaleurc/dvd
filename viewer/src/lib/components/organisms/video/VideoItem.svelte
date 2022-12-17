e8947d9983a250d84a2510c69fcb2a69189cb34a
Wei-Cheng Pan <legnaleurc@gmail.com>
2022-12-17 20:13:12 +0900

    tweak layout

--- viewer/src/lib/components/organisms/video/VideoItem.svelte
+++ viewer/src/lib/components/organisms/video/VideoItem.svelte
@@ -21,17 +21,17 @@
   }
 </script>
 
-<div class="w-full p-3 flex">
-  <div class="flex-1 flex flex-col break-all">
+<div class="w-full flex">
+  <div class="flex-1 p-3 flex flex-col break-all">
     <div>{video.name}</div>
     <div class="text-symbol-hint">{video.path}</div>
   </div>
-  <div class="flex-0 flex">
-    <IconButton on:click={handleOpen}>
-      <Icon name="open_in_new" />
-    </IconButton>
+  <div class="flex-0 flex flex-col">
     <IconButton on:click={handlePush}>
       <Icon name="chevron_right" />
+    </IconButton>
+    <IconButton on:click={handleOpen}>
+      <Icon name="open_in_new" />
     </IconButton>
   </div>
 </div>
