4b099f3f8098b236e44c099bdf30a0b868b13c45
Wei-Cheng Pan <legnaleurc@gmail.com>
2022-12-18 17:01:12 +0900

    remove svelte-preprocess

--- viewer/svelte.config.js
+++ viewer/svelte.config.js
@@ -1,11 +1,9 @@
 import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter({
