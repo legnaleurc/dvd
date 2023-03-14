346b427965529767ba81bc080f8b32dba8560f65
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-03-14 20:12:31 +0900

    fix message

--- engine/engine/api.py
+++ engine/engine/api.py
@@ -78,7 +78,7 @@ class NodeView(
 
     async def destory(self):
         node = await self.get_object()
-        getLogger(__name__).info(f"trasg {node.name}")
+        getLogger(__name__).info(f"trash `{node.name}` `{node.id_}`")
         drive: Drive = self.request.app["drive"]
         se: SearchEngine = self.request.app["se"]
         path = await drive.get_path(node)
