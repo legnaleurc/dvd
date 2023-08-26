90bc666e4931adcb57df2ffb602644fe5211bac8
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-08-26 13:57:01 +0900

    update deprecated type

--- viewer/src/lib/types/traits.ts
+++ viewer/src/lib/types/traits.ts
@@ -1,10 +1,10 @@
-import type { SvelteComponentTyped } from "svelte";
+import type { SvelteComponent } from "svelte";
 
 export type SvelteComponentConstructor<
   Props extends Record<string, unknown> = Record<string, unknown>,
   Events extends Record<string, unknown> = Record<string, unknown>,
   Slots extends Record<string, unknown> = Record<string, unknown>,
-> = new (...args: unknown[]) => SvelteComponentTyped<Props, Events, Slots>;
+> = new (...args: unknown[]) => SvelteComponent<Props, Events, Slots>;
 
 export type SvelteComponentModule<
   Props extends Record<string, unknown> = Record<string, unknown>,
