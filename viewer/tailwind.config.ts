d725beda5a6d174f3823e37cd9164b5e486de1e2
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-10-10 01:09:21 +0900

    switch to typescript

--- viewer/tailwind.config.ts
+++ viewer/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {
@@ -21,4 +22,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+} satisfies Config;
