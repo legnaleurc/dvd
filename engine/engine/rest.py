4a1768ecb3d4bfef45e6ab737ee0e0b4ffa1f31c
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-12-29 02:10:28 +0900

    tiny update

--- engine/engine/rest.py
+++ engine/engine/rest.py
@@ -1,76 +1,76 @@
-import abc
 import json
+from abc import ABCMeta, abstractmethod
 from typing import Any
 
 from aiohttp.web import Response
 from aiohttp.web_exceptions import HTTPNoContent
 
 
-class PermissionMixin(abc.ABC):
+class PermissionMixin(metaclass=ABCMeta):
     async def has_permission(self) -> bool:
         return True
 
-    @abc.abstractmethod
+    @abstractmethod
     async def raise_permission_error(self):
         ...
 
 
-class RetriveAPIMixin(PermissionMixin, abc.ABC):
+class RetriveAPIMixin(PermissionMixin, metaclass=ABCMeta):
     async def get(self):
         if not await self.has_permission():
             await self.raise_permission_error()
         rv = await self.retrive()
         return json_response(rv, status=200)
 
-    @abc.abstractmethod
+    @abstractmethod
     async def retrive(self):
         ...
 
 
-class ListAPIMixin(PermissionMixin, abc.ABC):
+class ListAPIMixin(PermissionMixin, metaclass=ABCMeta):
     async def get(self):
         if not await self.has_permission():
             await self.raise_permission_error()
         rv = await self.list_()
         return json_response(rv, status=200)
 
-    @abc.abstractmethod
+    @abstractmethod
     async def list_(self):
         ...
 
 
-class PartialUpdateAPIMixin(PermissionMixin, abc.ABC):
+class PartialUpdateAPIMixin(PermissionMixin, metaclass=ABCMeta):
     async def patch(self):
         if not await self.has_permission():
             await self.raise_permission_error()
         rv = await self.partial_update()
         return json_response(rv, status=200)
 
-    @abc.abstractmethod
+    @abstractmethod
     async def partial_update(self):
         ...
 
 
-class CreateAPIMixin(PermissionMixin, abc.ABC):
+class CreateAPIMixin(PermissionMixin, metaclass=ABCMeta):
     async def post(self):
         if not await self.has_permission():
             await self.raise_permission_error()
         rv = await self.create()
         return json_response(rv, status=201)
 
-    @abc.abstractmethod
+    @abstractmethod
     async def create(self):
         ...
 
 
-class DestroyAPIMixin(PermissionMixin, abc.ABC):
+class DestroyAPIMixin(PermissionMixin, metaclass=ABCMeta):
     async def delete(self):
         if not await self.has_permission():
             await self.raise_permission_error()
         await self.destory()
         raise HTTPNoContent()
 
-    @abc.abstractmethod
+    @abstractmethod
     async def destory(self):
         ...
 
