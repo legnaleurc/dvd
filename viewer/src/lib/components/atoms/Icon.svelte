2490fe1eb3ddeb3ccab5b817321772265542cc2e
Wei-Cheng Pan <legnaleurc@gmail.com>
2024-01-05 19:13:05 +0900

    feat: remove unused property

--- viewer/src/lib/components/atoms/Icon.svelte
+++ viewer/src/lib/components/atoms/Icon.svelte
@@ -1,19 +1,5 @@
-<script lang="ts" context="module">
-  type IconStyle = "filled";
-
-  function getIconStyle(style: string) {
-    if (!style || style === "filled") {
-      return "material-icons";
-    }
-    return `material-icons-${style}`;
-  }
-</script>
-
 <script lang="ts">
   export let name: string;
-  export let style: IconStyle = undefined;
-
-  $: iconStyle = getIconStyle(style);
 </script>
 
 <span class="material-symbols-outlined">{name}</span>
