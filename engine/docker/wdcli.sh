06db4911f6050ca73f67b9fbad50a0cadd1fafe0
Wei-Cheng Pan <legnaleurc@gmail.com>
2024-02-28 18:36:02 +0900

    chore: non root util

--- engine/docker/wdcli.sh
+++ engine/docker/wdcli.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
 
-poetry run -- python3 -m wcpan.drive.cli --config="$DVD_ENGINE_DRIVE" "$@"
+if [ -z "$DVD_ENGINE_UID" ] || [ -z "$DVD_ENGINE_GID" ] ; then
+    exit 1
+fi
+
+exec setpriv \
+    --reuid="$DVD_ENGINE_UID" \
+    --regid="$DVD_ENGINE_GID" \
+    --groups="$DVD_ENGINE_GID" \
+    poetry run -- \
+    python3 -m wcpan.drive.cli --config="$DVD_ENGINE_DRIVE" "$@"
