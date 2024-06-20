3248dc317ae0f7788ae87e49ef0a1d3495e032f0
Wei-Cheng Pan <legnaleurc@gmail.com>
2024-06-21 01:50:40 +0900

    fix: remove platform hack

--- unpack/src/global.cpp
+++ unpack/src/global.cpp
@@ -1,14 +1,10 @@
 #include "global.hpp"
 
-#ifndef _WIN32
-#include <locale.h>
-#endif
+#include <clocale>
 
 void
 unpack::initialize()
 {
   // libarchive/libarchive#587
-#ifndef _WIN32
-  setlocale(LC_ALL, "");
-#endif
+  std::setlocale(LC_ALL, "");
 }
