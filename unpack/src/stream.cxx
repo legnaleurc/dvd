57dd0f6c588dc70254ebae51eade2aab98038ce5
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-08-20 22:37:49 +0900

    chore: refactor

--- unpack/src/stream.cxx
+++ unpack/src/stream.cxx
@@ -2,22 +2,12 @@
 #include "stream_file.hxx"
 #include "stream_http.hxx"
 
-namespace {
-
-bool
-starts_with(const std::string& str, const std::string& prefix)
-{
-  return str.rfind(prefix, 0) == 0;
-}
-
-}
-
 namespace unpack {
 
 std::unique_ptr<input_stream::detail>
 input_stream::detail::create(const std::string& uri)
 {
-  if (starts_with(uri, "http://") || starts_with(uri, "https://")) {
+  if (uri.starts_with("http://") || uri.starts_with("https://")) {
     return std::make_unique<http_detail>(uri);
   } else {
     return std::make_unique<file_detail>(uri);
