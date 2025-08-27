baf0d34bf1372616612524928aa304f665c59837
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-08-28 01:10:52 +0900

    feat: migrate to rootless podman

--- engine/docker/start.sh
+++ engine/docker/start.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 
-if [ -z "$DVD_ENGINE_UID" ] || [ -z "$DVD_ENGINE_GID" ] ; then
-    exit 1
-fi
-
 CMD="poetry run -- python3 -m engine"
 CMD="$CMD -H 0.0.0.0"
 CMD="$CMD -p $DVD_ENGINE_PORT"
@@ -16,8 +12,4 @@ fi
 
 export TMPDIR="$DVD_ENGINE_TMP"
 
-exec setpriv \
-    --reuid="$DVD_ENGINE_UID" \
-    --regid="$DVD_ENGINE_GID" \
-    --groups="$DVD_ENGINE_GID" \
-    $CMD
+exec $CMD
