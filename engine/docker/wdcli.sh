d39e5520ad88d65c9119b37817648c952dff72af
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-12-10 12:57:44 +0900

    feat: remove setuid

--- engine/docker/wdcli.sh
+++ engine/docker/wdcli.sh
@@ -1,12 +1,3 @@
 #! /bin/sh
 
-if [ -z "$DVD_ENGINE_UID" ] || [ -z "$DVD_ENGINE_GID" ] ; then
-    exit 1
-fi
-
-exec setpriv \
-    --reuid="$DVD_ENGINE_UID" \
-    --regid="$DVD_ENGINE_GID" \
-    --groups="$DVD_ENGINE_GID" \
-    poetry run -- \
-    python3 -m wcpan.drive.cli --config="$DVD_ENGINE_DRIVE" "$@"
+exec poetry run -- python3 -m wcpan.drive.cli --config="$DVD_ENGINE_DRIVE" "$@"
