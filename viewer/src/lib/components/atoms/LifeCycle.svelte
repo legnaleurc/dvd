ce739794acb3f187eb80b6222d2999fb4136c1ed
Wei-Cheng Pan <legnaleurc@gmail.com>
2026-04-27 10:36:38 +0900

    feat: add visibility listener

--- viewer/src/lib/components/atoms/LifeCycle.svelte
+++ viewer/src/lib/components/atoms/LifeCycle.svelte
@@ -36,4 +36,39 @@
 
     return isMounted;
   }
+
+  export function useVisibilityChange() {
+    function handleVisibilityChange() {
+      if (document.hidden) {
+        hiddenHandlers.forEach((fn) => fn());
+      } else {
+        visibleHandlers.forEach((fn) => fn());
+      }
+    }
+
+    const hiddenHandlers: Array<() => void> = [];
+    function onHidden(handler: () => void) {
+      hiddenHandlers.push(handler);
+    }
+
+    const visibleHandlers: Array<() => void> = [];
+    function onVisible(handler: () => void) {
+      visibleHandlers.push(handler);
+    }
+
+    onMount(() => {
+      document.addEventListener("visibilitychange", handleVisibilityChange);
+      return () => {
+        document.removeEventListener(
+          "visibilitychange",
+          handleVisibilityChange,
+        );
+      };
+    });
+
+    return {
+      onHidden,
+      onVisible,
+    };
+  }
 </script>
